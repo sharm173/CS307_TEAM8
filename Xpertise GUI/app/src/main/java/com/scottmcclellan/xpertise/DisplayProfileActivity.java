debdac3db300c8d3b196323e9e6df3c534cef4c7
Josh Foeh <jfoeh@purdue.edu>
2015-10-18 15:39:29 -0400

    fixed the gitignore, DO NOT CHANGE THE GITIGNORE

--- Xpertise GUI/app/src/main/java/com/scottmcclellan/xpertise/DisplayProfileActivity.java
+++ Xpertise GUI/app/src/main/java/com/scottmcclellan/xpertise/DisplayProfileActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -54,4 +55,10 @@ public class DisplayProfileActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
 }
