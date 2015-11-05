c0e03c33d4e4961a9fc082481907f7366ee36fb6
Trevor Edris <trevor.edris@gmail.com>
2015-11-04 22:33:45 -0500

    ACTUALLY made changes to databaseConnection with methods for the API calls

--- Xpertise GUI/backend/src/main/java/com/example/Scott/myapplication/backend/databaseConnection.java
+++ Xpertise GUI/backend/src/main/java/com/example/Scott/myapplication/backend/databaseConnection.java
@@ -101,8 +101,7 @@ public class databaseConnection {
         if (success == 1) {
             bean.setBool(true);
             bean.setData("Successful insert");
-        }
-        else {
+        } else {
             databaseError(Constants.DB_ERROR.INSERT_ERROR);
             bean.setBool(false);
             bean.setData("Error: Inserting into database failed");
@@ -349,5 +348,206 @@ public class databaseConnection {
             e.printStackTrace();
         }
         return ret;
+    }
+
+    //TODO: Make all these work
+    //TODO: Ensure all relevant MyBean fields are populated with correct data
+
+    public ArrayList<Profile> profilesInRadius(int pid, double dist) {
+        ArrayList<Profile> profiles = new ArrayList<Profile>();
+        String url = Constants.DATABASE_URL;
+
+        try{
+            Class.forName(Constants.GOOGLE_DRIVER);
+            Connection conn = DriverManager.getConnection(url);
+
+            try{
+                Profile profile = getSpecificProfile(pid);
+                MyBean latLngHolder = Search.boundingBox(profile.getLat(), profile.getLng(), dist);
+                double hiLat = latLngHolder.getHiLat();
+                double loLat = latLngHolder.getLoLat();
+                double hiLng = latLngHolder.getHiLng();
+                double loLng = latLngHolder.getLoLng();
+
+                //TODO: Figure out how to do this SQL magic here
+                String statement = "";
+                PreparedStatement stmt = conn.prepareStatement(statement);
+
+                ResultSet response = stmt.executeQuery();
+                while(response.next()){
+                    Profile temp = new Profile();
+                    temp.setPid(response.getInt("pid"));
+                    temp.setDescription(response.getString("description"));
+                    temp.setFirstName(response.getString("firstName"));
+                    temp.setLastName(response.getString("lastName"));
+                    temp.setPassword(response.getString("password"));
+                    temp.setEmail(response.getString("email"));
+                    temp.setCity(response.getString("city"));
+                    temp.setLat(response.getDouble("lat"));
+                    temp.setLng(response.getDouble("lng"));
+                    profiles.add(temp);
+                }
+
+            }finally{
+                conn.close();
+            }
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        return profiles;
+    }
+
+    public static MyBean postReview(int reviewerPid, int revieweePid, int stars, String desc){
+        String url = Constants.DATABASE_URL;
+        MyBean bean = new MyBean();
+        int success = 0;
+
+        try{
+            Class.forName(Constants.GOOGLE_DRIVER);
+            Connection conn = DriverManager.getConnection(url);
+
+            try{
+
+                String statement = "INSERT INTO reviews (reviewer, reviewee, stars, desc) " +
+                        "VALUES ('" +
+                        Integer.toString(reviewerPid) + "', '" +
+                        Integer.toString(revieweePid) + "', '" +
+                        Integer.toString(stars) + "', '" +
+                        desc + "')";
+                PreparedStatement stmt = conn.prepareStatement(statement);
+
+                success = stmt.executeUpdate();
+
+            }finally{
+                conn.close();
+            }
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        if(success == 1){
+            bean.setBool(true);
+            bean.setData("Successful insert");
+        }else{
+            bean.setBool(false);
+            bean.setData("Error: Inserting into database failed");
+        }
+
+        return bean;
+    }
+
+    public static ArrayList<Review> getReviews(int pid){
+        String url = Constants.DATABASE_URL;
+        ArrayList<Review> reviews = new ArrayList<Review>();
+
+        try {
+            Class.forName(Constants.GOOGLE_DRIVER);
+            Connection conn = DriverManager.getConnection(url);
+
+            try {
+
+                String statement = "SELECT * FROM review WHERE pid=(?)";
+                PreparedStatement stmt = conn.prepareStatement(statement);
+                stmt.setString(1, Integer.toString(pid));
+
+                ResultSet response = stmt.executeQuery();
+                while(response.next()) {
+                    // Set data for myBean
+                    Review temp = new Review();
+
+                    //TODO: Make sure column names are correct
+                    temp.setReviewDesc(response.getString("description"));
+                    temp.setReviewee_pid(response.getInt("reviewee"));
+                    temp.setReviewer_pid(response.getInt("reviewer"));
+                    temp.setStars(response.getInt("stars"));
+                    reviews.add(temp);
+                }
+
+            }finally{
+                conn.close();
+            }
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        return reviews;
+    }
+
+    public static MyBean setTag(int pid, String tag){
+        String url = Constants.DATABASE_URL;
+        MyBean bean = new MyBean();
+        int success = 0;
+
+        try{
+            Class.forName(Constants.GOOGLE_DRIVER);
+            Connection conn = DriverManager.getConnection(url);
+
+            try{
+
+                String statement = "INSERT INTO table (pid, tag) VALUES ('" +
+                        Integer.toString(pid) + "', '" +
+                        tag + "')";
+                PreparedStatement stmt = conn.prepareStatement(statement);
+
+                success = stmt.executeUpdate();
+
+
+            }finally{
+
+            }
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        if(success == 1){
+            bean.setBool(true);
+            bean.setData("Successful insert");
+        }else{
+            bean.setBool(false);
+            bean.setData("Error: Inserting into database failed");
+        }
+
+        return bean;
+    }
+
+    public static ArrayList<MyBean> getTags(int pid){
+        String url = Constants.DATABASE_URL;
+        ArrayList<MyBean> beans = new ArrayList<MyBean>();
+
+        try{
+            Class.forName(Constants.GOOGLE_DRIVER);
+            Connection conn = DriverManager.getConnection(url);
+
+            try{
+
+                String statement = "SELECT * FROM tags WEHRE pid=(?)";
+                PreparedStatement stmt = conn.prepareStatement(statement);
+                stmt.setString(1, Integer.toString(pid));
+
+                ResultSet response = stmt.executeQuery();
+                while(response.next()){
+                    MyBean temp = new MyBean();
+
+                    //TODO: Make sure column names are correct
+                    temp.setData(response.getString("tag"));
+                    temp.setPid(pid);
+                    beans.add(temp);
+                }
+
+            }finally{
+                conn.close();
+            }
+
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        return beans;
     }
 }
