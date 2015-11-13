54ff8eb973c4dea69ff566b4968188dc409c7a78
Tushar <sharm173@purdue.edu>
2015-11-12 22:20:31 -0500

    SelectedUserActivity and Ratings

--- Xpertise GUI/app/src/main/java/com/scottmcclellan/xpertise/SelectedUserActivity.java
+++ Xpertise GUI/app/src/main/java/com/scottmcclellan/xpertise/SelectedUserActivity.java
@@ -28,11 +28,12 @@ import java.io.IOException;
  */
 public class SelectedUserActivity extends AppCompatActivity {
     private Profile profile;
+    //TODO: profile object to be fetched from list
 
     private TextView first;
     private TextView last;
     private TextView email;
-    private TextView pass;
+   // private TextView pass;
     private TextView city;
     //    private TextView lat;
 //    private TextView lng;
@@ -41,6 +42,7 @@ public class SelectedUserActivity extends AppCompatActivity {
     private Button login;
     private Button edit;
     private Button find;
+    private TextView comment;
     Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,41 +52,34 @@ public class SelectedUserActivity extends AppCompatActivity {
         first  = (TextView) findViewById(R.id.firstName);
         last = (TextView) findViewById(R.id.lastName);
         email = (TextView) findViewById(R.id.email);
-        pass = (TextView) findViewById(R.id.password);
+        //pass = (TextView) findViewById(R.id.password);
         city = (TextView) findViewById(R.id.city);
 
         desc = (TextView) findViewById(R.id.desc);
         rating = (RatingBar) findViewById(R.id.ratingBar);
+        comment = (TextView) findViewById(R.id.editText);
        // login = (Button) findViewById(R.id.login);
 
         first.setText(profile.getFirstName());
         last.setText(profile.getLastName());
         email.setText(profile.getEmail());
-        pass.setText(profile.getPassword());
+       // pass.setText(profile.getPassword());
         city.setText(profile.getCity());
         desc.setText(profile.getDescription());
+
+        //call user get ratings task - populate list view inside it.
+
+
     }
 
-    public class UserRegisterTask extends AsyncTask<Void, Void, Boolean> {
-        private final String mFirst;
-        private final String mLast;
-        private final String mEmail;
-        private final String mPass;
-        private final String mCity;
-        private final Double mLat;
-        private final Double mLng;
-        private final String mDes;
+    public class UserSetRatingTask extends AsyncTask<Void, Void, Boolean> {
+        private final String comments;
+        private final int rate;
 
 
-        UserRegisterTask(Profile p) {
-            mFirst = p.getFirstName();
-            mLast = p.getLastName();
-            mEmail = p.getEmail();
-            mPass = p.getPassword();
-            mCity = p.getCity();
-            mLat = p.getLat();
-            mLng = p.getLng();
-            mDes = p.getDescription();
+        UserSetRatingTask(Profile p) {
+            comments = comment.getText().toString();
+            rate =  rating.getNumStars();
         }
 
         @Override
@@ -115,15 +110,9 @@ public class SelectedUserActivity extends AppCompatActivity {
             try {
                 //Simulate network access.
                 //Thread.sleep(2000);
-                MyBean b = myApiService.profilePost(mFirst,mLast,mPass, mEmail, mCity, mLat, mLng, mDes).execute();
+                MyBean b = myApiService.profilePostReview(LoginActivity.loggedInProfile.getPid(), profile.getPid(), rate, comments).execute();
 
-                //TODO:change to profile activity if p is not null. P is saved in global profile object- 'user'
 
-                // Log.e("First name is: ", p.getFirstName() + ", last name is: " + p.getLastName());
-
-                //  if(b.getBool()) {
-
-                // }
 
                 return b.getBool();
 
@@ -155,7 +144,102 @@ public class SelectedUserActivity extends AppCompatActivity {
               //  Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
               //  startActivity(intent);
 
+                Toast.makeText(SelectedUserActivity.this, "Review submitted", Toast.LENGTH_SHORT).show();
+                finish();
+                //     Intent i = new Intent(context, DisplayProfileActivity.class);
+                //    Bundle bundle = new Bundle();
+                //    bundle.putSerializable("profile", user);
+                //    i.putExtras(bundle);
+                //    startActivity(i);
+                //    finish();
 
+            } else {
+                //password.setError(getString(R.string.error_incorrect_password));
+                //password.requestFocus();
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+//            // showProgress(false);
+        }
+
+
+    }
+
+    public class UserGetRatingsTask extends AsyncTask<Void, Void, Boolean> {
+        private final String comments;
+        private final int rate;
+
+
+        UserGetRatingsTask(Profile p) {
+            comments = comment.getText().toString();
+            rate =  rating.getNumStars();
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            // TODO: attempt authentication against a network service.
+            Context context;
+            XpertiseAPI myApiService = null;
+
+            if(myApiService == null) {  // Only do this once
+                XpertiseAPI.Builder builder = new XpertiseAPI.Builder(AndroidHttp.newCompatibleTransport(),
+                        new AndroidJsonFactory(), null)
+                        // options for running against local devappserver
+                        // - 10.0.2.2 is localhost's IP address in Android emulator
+                        // - turn off compression when running against local devappserver
+                        .setRootUrl("https://xpertiseservergae.appspot.com/_ah/api")
+                        .setApplicationName("xpertise")
+                        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                            @Override
+                            public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                                abstractGoogleClientRequest.setDisableGZipContent(true);
+                            }
+                        });
+
+                myApiService = builder.build();
+            }
+
+
+            try {
+                //Simulate network access.
+                //Thread.sleep(2000);
+                MyBean b = myApiService.profilePostReview(LoginActivity.loggedInProfile.getPid(), profile.getPid(), rate, comments).execute();
+
+
+
+                return b.getBool();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                return false;
+            }
+
+            // TODO: register the new account here.
+            //Need profile details to register, switch to new activity to register
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            // mAuthTask = null;
+            // showProgress(false);
+
+            Log.e("Succes is: ", Boolean.toString(success));
+//LoginActivity.this
+            if (success) {
+                //LinkedHashMap<String, Object> obj = new LinkedHashMap<String, Object>();
+                // obj.put("hashmapkey", user);
+                // Intent i = new Intent(context, LoginActivity.class);
+                //       Bundle b = new Bundle();
+                //     b.putSerializable("bundleobj", user);
+                //   i.putExtra("profile",b);
+                // startActivity(i);
+                //    Toast.makeText(RegisterActivity.this, "Your profile has been successfully created!", Toast.LENGTH_SHORT).show();
+                //  Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                //  startActivity(intent);
+
+                Toast.makeText(SelectedUserActivity.this, "Review submitted", Toast.LENGTH_SHORT).show();
                 finish();
                 //     Intent i = new Intent(context, DisplayProfileActivity.class);
                 //    Bundle bundle = new Bundle();
